945abe7a28481b52d6b451dd4d98ffff0dd41afd
Jens Kristian Espevik <jenskristian@gmail.com>
2018-10-10 13:30:16 +0200

    finish removeFromArray function

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    let array = arguments[0];
+    let result = [];
 
+    // Iterate through however many arguments there are
+    for (let i = 1; i <= arguments.length; i++) {
+        // Iterate through the array in argument[0] and compare with other arguments
+        for (let n = 0; n < array.length; n++) {
+            // Use splice() to remove the item and readjust the index.
+            if (arguments[i] == array[n]) {
+                array.splice(n, 1);
+            }
+        }
+    }
+    return array;
 }
 
 module.exports = removeFromArray
