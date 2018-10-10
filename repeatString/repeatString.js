2a25ff03bcdd5e3dbcbff5a29d086c11f8341def
Jens Kristian Espevik <jenskristian@gmail.com>
2018-10-10 08:22:17 +0200

    finish repeat function according to spec tests

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(hey, num) {
+    finalString = hey;
+    if (num === 0) {
+        return "";
+    }
+    if (num < 0) {
+        return "ERROR"
+    }
+    for (let i = 1; i < num; i++) {
+        finalString += hey;
+        
+    }
+    return finalString;
 }
 
 module.exports = repeatString
