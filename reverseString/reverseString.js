73be71b245415a11b6aa3ff121935224ab7bc0f9
Jens Kristian Espevik <jenskristian@gmail.com>
2018-10-10 11:29:56 +0200

    complete reverseString method

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let rev_string = "";
+    for (i = string.length; i >= 0; i--) {
+        rev_string += string.charAt(i);
+    }
+    return rev_string;
 }
 
 module.exports = reverseString
